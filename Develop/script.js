19d6fdf35acfd878850f358b112b1f12cd65ac78
Brahm V <brahmvanh@gmail.com>
2022-12-30 13:51:45 -0500

    Final review on script and application otherwise. Added a README file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,15 @@
+// Used day.js to display current date at top of application
+
 var currentDay = dayjs().format('MMM D, YYYY')
 $('#currentDay').text(currentDay);
 
+// All of the application's functions are nested inside a .ready() method 
+//to ensure that all of the code has been processed on page load before running.
+
 $(document).ready(function() {
+
+// Used to save user's entered content in a time block to local storage
+
 
     // To save user's logged event in local storage
     $('.saveBtn').on('click', function() {
@@ -12,22 +20,16 @@ $(document).ready(function() {
 
     })
 
-    $('.saveBtn').on('dblclick', function() {
-
-      userEntry = $(this).siblings('.description').val();
-      timeKey = $(this).parent().attr('id');
-      localStorage.removeItem(timeKey, userEntry);
-    
-    })
-
+// Used to accurately track the current time of day
 
     function timeTracker () {
       
-      // current time variable to be embedded into comparison function 
+     
+      
       var currentTime = dayjs().hour();
       console.log(currentTime);
       
-      // function to parse time ID from time-block... to be later compared to current time
+      // Function to parse time ID from time-block... to be later compared to current time
       
       $('.time-block').each(function() {
         
@@ -36,6 +38,7 @@ $(document).ready(function() {
         
       
         // If statement to change classes of time blocks and their displayed colors
+        
         if (timeId < currentTime) {
           
           $(this).removeClass('future');
@@ -61,6 +64,8 @@ $(document).ready(function() {
       })
     }
 
+    // Used to retrieve saved schedule data from local browser storage
+
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
     $('#hour-10 .description').val(localStorage.getItem('hour-10'));
     $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -70,6 +75,8 @@ $(document).ready(function() {
     $('#hour-15 .description').val(localStorage.getItem('hour-15'));
     $('#hour-16 .description').val(localStorage.getItem('hour-16'));
     $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+    // Call to run timeTracker function
 
     timeTracker();
 
